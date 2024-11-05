1501fd9b7c2323c27baa470530a32a1869646c00
JhonBG <jhon.botero1@udea.edu.co>
2024-11-05 09:33:20 -0500

    add inventory price

--- src/Product.php
+++ src/Product.php
@@ -2,13 +2,20 @@
 
 namespace App;
 
+use App\ValueObject\Money;
+
 class Product
 {
     public function __construct(
-        readonly public string $name
+        readonly public string $name,
+        public Money $price
     )
     {
         //
     }
 
+    public function getPrice(): Money
+    {
+        return $this->price;
+    }
 }
